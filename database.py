97e42cc41274ef87e4d4d0a9b50434e2796a3c9c
Seth <61952590+xiancicici@users.noreply.github.com>
2024-03-17 23:43:02 +0000

    refactor db pass

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://avnadmin:AVNS_PiD51pD5xwoJxwnfFmd@cici-berkeley-e360.a.aivencloud.com:25746/cicidb?charset=utf8mb4"
+AIVEN_DB_PASS = os.getenv('AIVEN_DB_PASS')
+db_connection_string = f"mysql+pymysql://avnadmin:{AIVEN_DB_PASS}@cici-berkeley-e360.a.aivencloud.com:25746/cicidb?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem"
                        }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
