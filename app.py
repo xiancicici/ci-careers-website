8b4830b4f340e8c557c4d97add9cf982f27f0ffd
Seth <61952590+xiancicici@users.noreply.github.com>
2024-03-18 00:00:56 +0000

    update code

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ from sqlalchemy import create_engine, text
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def front_page():
-    return render_template('home.html', 
-                           jobs=load_jobs_from_db(), 
-                           company_name='Cicify')
+  return render_template('home.html',
+                         jobs=load_jobs_from_db(),
+                         company_name='Cicify')
+
 
 @app.route("/api/jobs")
-def list_jobs():
-  return jsonify(load_jobs_from_db())
+def get_list_jobs():
+  jobs = load_jobs_from_db()
+  return jsonify(data=jobs, status=200, mimetype='application/json')
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 import os
+import json
 
 AIVEN_DB_PASS = os.getenv('AIVEN_DB_PASS')
 db_connection_string = f"mysql+pymysql://avnadmin:{AIVEN_DB_PASS}@cici-berkeley-e360.a.aivencloud.com:25746/cicidb?charset=utf8mb4"
@@ -13,4 +14,5 @@ engine = create_engine(db_connection_string,
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs")).mappings().all()
-    return result
+    jobs = [dict(row) for row in result]
+    return jobs
