76dede8c0c63062dddc67431ec50e5009d674b67
Seth <61952590+xiancicici@users.noreply.github.com>
2024-03-19 03:12:52 +0000

    add job page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
@@ -16,6 +16,12 @@ def front_page():
 def get_list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(data=jobs, status=200, mimetype='application/json')
+
+
+@app.route("/job/<id>")
+def get_job(id):
+  job = load_job_from_db(id)
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -16,3 +16,13 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs")).mappings().all()
     jobs = [dict(row) for row in result]
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from jobs where id = {id}"))
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+    
