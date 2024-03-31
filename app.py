f459386dd9d51dc884c415e031cfbd4ac05d111e
Seth <61952590+xiancicici@users.noreply.github.com>
2024-03-31 23:08:21 +0000

    add submit page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 from sqlalchemy import create_engine, text
 
@@ -22,6 +22,14 @@ def get_list_jobs():
 def get_job(id):
   job = load_job_from_db(id)
   return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=["post"])
+def apply_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  return render_template('application_submited.html', application=data, job=job)
+  
+
 
 
 if __name__ == '__main__':
